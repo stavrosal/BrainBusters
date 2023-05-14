7731ab064d6a0b53cf96378088d653b996db1fba
Stavros Almpanopoulos <ics22056@uom.edu.gr>
2023-05-14 19:28:05 +0300

    Changes to all classes, imporved UI and added button functionallity

--- src/SingleGame.java
+++ src/SingleGame.java
@@ -2,25 +2,30 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
+import javax.swing.Timer;
 
 public class SingleGame extends JFrame{
 	private JPanel mainpanel = new JPanel();
 	private JPanel questionp = new JPanel(new FlowLayout());
-	private JPanel ansp1 = new JPanel(new FlowLayout());
-	private JPanel ansp2 = new JPanel(new FlowLayout());
+	private JPanel ansp1 = new JPanel();
+	private JPanel ansp2 = new JPanel();
 	private JPanel quitPanel = new JPanel();
+	private JLabel correctAns = new JLabel();
 	private JLabel lab = new JLabel();
 	private JLabel lab2 = new JLabel();
 	private JButton option1 = new JButton();
@@ -32,10 +37,9 @@ public class SingleGame extends JFrame{
 			
 	
 	public SingleGame(int count, int corr, int wrong) throws IOException {
-
 		
 		ArrayList<Question> questions = new ArrayList<Question>();
-		String link = "https://www.quiztriviagames.com/multiple-choice-trivia-questions/";
+		String link = "https://www.quiztriviagames.com/multiple-choice-trivia-questions/"; //Web-site to scrape the questions from
 		
 		Scrape test = new Scrape(link, questions);
 		test.ScrapeQuestions();
@@ -44,15 +48,17 @@ public class SingleGame extends JFrame{
 		questionp.setBorder(grayline);
 			
 		
-		option1.setPreferredSize(new Dimension(400, 50));
-		option2.setPreferredSize(new Dimension(400, 50));
-		option3.setPreferredSize(new Dimension(400, 50));
-		option4.setPreferredSize(new Dimension(400, 50));
-		quit.setPreferredSize(new Dimension(200, 50));
+		option1.setPreferredSize(new Dimension(400, 150));
+		option2.setPreferredSize(new Dimension(400, 150));
+		option3.setPreferredSize(new Dimension(400, 150));
+		option4.setPreferredSize(new Dimension(400, 150));
+		quit.setMaximumSize(new Dimension(300, 200));
 		
 		
 		lab.setFont(new Font("Calibri", Font.BOLD, 30));
 		lab2.setFont(new Font("Calibri", Font.BOLD, 50));
+		correctAns.setFont(new Font("Calibri", Font.BOLD, 30));
+		
 		
 		questionp.add(lab);
 		ansp1.add(option1);
@@ -60,13 +66,21 @@ public class SingleGame extends JFrame{
 		ansp2.add(option3);
 		ansp2.add(option4);
 		
+		quit.setText("QUIT");
+		quit.setAlignmentX(Component.CENTER_ALIGNMENT); //Center the labels and the button
+		lab2.setAlignmentX(Component.CENTER_ALIGNMENT);
+		correctAns.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
 		quitPanel.add(lab2);
+		quitPanel.add(correctAns); //label which will show the correct answer (triggered by button press)
+		quitPanel.add(Box.createVerticalGlue()); //this will make some distance between the corr/wrong label and the quit button
 		quitPanel.add(quit);
+		quitPanel.setLayout(new BoxLayout(quitPanel, BoxLayout.PAGE_AXIS));
 		
-		
-		
-
+	
 		Question q = questions.get(count);
+		
+		
 		lab.setText(q.getQuestion());
 			
 		ArrayList<String> answers = q.getAnswers();
@@ -75,9 +89,12 @@ public class SingleGame extends JFrame{
 		option2.setText(answers.get(1));
 		option3.setText(answers.get(2));
 		option4.setText(answers.get(3));
-		quit.setText("QUIT");
+				
 		
-		ButtonListener listener = new ButtonListener(q.getCorrectAnswer(), count, this, corr, wrong);
+		
+		
+		
+		ButtonListener listener = new ButtonListener(q.getCorrectAnswer(), count, questions.size(), this, corr, wrong); //passing the stats to the next window
 		option1.addActionListener(listener);
 		option2.addActionListener(listener);
 		option3.addActionListener(listener);
@@ -88,21 +105,20 @@ public class SingleGame extends JFrame{
 		icon = new ImageIcon("logo.png");
 		setIconImage(icon.getImage());
 		
+		
 		mainpanel.add(questionp);
 		mainpanel.add(ansp1);
 		mainpanel.add(ansp2);
 		mainpanel.add(quitPanel);
-		
-		
-		
+		mainpanel.setLayout(new BoxLayout(mainpanel, BoxLayout.PAGE_AXIS));
 		
 		this.setContentPane(mainpanel);
 
 		this.setVisible(true);
 		this.setSize(1000, 800);
 		this.setTitle("BrainBusters");
-		this.setResizable(false);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	
 	}
 	
@@ -112,20 +128,49 @@ public class SingleGame extends JFrame{
 		private SingleGame window;
 		private int corr;
 		private int wrong;
+		private int max; //the number of questions in total
 		
-		public ButtonListener(String correct, int count, SingleGame window, int corr, int wrong) {
+		public ButtonListener(String correct, int count, int max, SingleGame window, int corr, int wrong) {
 			this.correct = correct;
 			this.count = count;
+			this.max = max;
 			this.window = window;
 			this.corr = corr;
 			this.wrong = wrong;
 		}
 		
+		public void DisableButtons() { //A function to disable the panel's button so the user won't double-click
+			window.option1.setEnabled(false);
+			window.option2.setEnabled(false);
+			window.option3.setEnabled(false);
+			window.option4.setEnabled(false);
+		}
+		
 		public void actionPerformed(ActionEvent e) {
 			String ans = ((JButton) e.getSource()).getText();
-
+			
+			
+			ActionListener listen = new ActionListener() { //Listener for the timer, it opens a new window for the question
+				@Override                                  //and closes the old one
+				public void actionPerformed(ActionEvent e) {
+					window.dispose();
+					try {
+						if (count+1 < max)
+							
+							new SingleGame(count+1, corr, wrong);
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+			};
+			
+			Timer timer = new Timer(1500, listen); //Creates the timer with 1.5 second wait
+			timer.setRepeats(false); //Makes the timer execute only once
+				
 			
 			if (e.getSource() == option1) {
+				DisableButtons();
+				timer.start();
 				if (ans.equals(correct)) {
 					window.lab2.setForeground(Color.GREEN);
 					window.lab2.setText("\nCORRECT!");
@@ -134,68 +179,64 @@ public class SingleGame extends JFrame{
 				else {
 					lab2.setForeground(Color.RED);
 					window.lab2.setText("WRONG!");	
+					window.correctAns.setText("Correct Answer: " + correct);
 					wrong++;
 				}
-				try {
-					//window.dispose();
-					new SingleGame(count+1, corr, wrong);
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
 			}
-			
+				
 			if (e.getSource() == option2) {
+				DisableButtons();
+				timer.start();
 				if (ans.equals(correct)) {
 					window.lab2.setForeground(Color.GREEN);
 					window.lab2.setText("\nCORRECT!");
+					corr++;
 				}
 				else {
 					lab2.setForeground(Color.RED);
-					window.lab2.setText("WRONG!");
-					try {
-						//window.dispose();
-						new SingleGame(count+1, corr, wrong);
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
+					window.lab2.setText("WRONG!");	
+					window.correctAns.setText("Correct Answer: " + correct);
+					wrong++;
 				}
 			}
+				
 			if (e.getSource() == option3) {
+				DisableButtons();
+				timer.start();
 				if (ans.equals(correct)) {
 					window.lab2.setForeground(Color.GREEN);
 					window.lab2.setText("\nCORRECT!");
+					corr++;
 				}
 				else {
 					lab2.setForeground(Color.RED);
-					window.lab2.setText("WRONG!");
-					try {
-						//window.dispose();
-						new SingleGame(count+1, corr, wrong);
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
+					window.lab2.setText("WRONG!");	
+					window.correctAns.setText("Correct Answer: " + correct);
+					wrong++;
 				}
 			}
+				
 			if (e.getSource() == option4) {
+				DisableButtons();
+				timer.start();
 				if (ans.equals(correct)) {
 					window.lab2.setForeground(Color.GREEN);
 					window.lab2.setText("\nCORRECT!");
+					corr++;
 				}
 				else {
 					lab2.setForeground(Color.RED);
-					window.lab2.setText("WRONG!");
-					try {
-						//window.dispose();
-						new SingleGame(count+1, corr, wrong);
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
+					window.lab2.setText("WRONG!");	
+					window.correctAns.setText("Correct Answer: " + correct);
+					wrong++;
 				}
+					
 			}
-			if (e.getSource() == quit) {
-				new EnterName(corr, wrong);
+			if (e.getSource() == quit || count+1 == max) {
+				new EnterName(corr, wrong, true);
 				window.dispose();
 			}
+			
 		}
 	}
 	
