cb6c1469ea89cfc2f5ac922cc2f47c9bd2443f0c
Stavros Almpanopoulos <102695313+stavrosal@users.noreply.github.com>
2023-05-30 17:07:47 +0300

    Minor Changes

--- src/SingleGame.java
+++ src/SingleGame.java
@@ -121,7 +121,8 @@ public class SingleGame extends JFrame{
 		this.setSize(1000, 800);
 		this.setTitle("BrainBusters");
 		//this.setResizable(false);
-		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		//this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE); //Prompts the user to use the QUIT button by disabling the X button on the upper right side
 	
 	}
 	
